e606933b1d1f216d97bfd438c9a7518c8516307c
Andrea <andrea.ramos.soto@angloaleman.edu.mx>
2022-10-28 12:38:25 -0500

    Actualizacion,metodos

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 // Agrega el objeto contador
-
-
-
-
+let contador = {
+  unidades:1,
+  decenas:4,
+  puntaje:0
+ incrementarPuntaje(){
+    contador.unidades++
+    return contador.puntaje
+  }
+decrementarPuntaje(){
+    contador.puntaje--
+    return contador.puntaje
+}
+}
 // Agrega el obeto juego
-
+let juego = {
+    timerId:0,
+    timerObstaculos:0,
+    gravedad:2
+    aleatorio(4, 5){
+      let aleatorio = (4, 5)
+        return Math.random(aleatorio)
+    }
+}
 
